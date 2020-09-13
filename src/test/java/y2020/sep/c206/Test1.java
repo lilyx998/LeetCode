b77d01c8a81427985b7362d81c4364b53fd6a447
Owner <Owner@local>
2020-09-13 19:29:06 -0400

    .

---
+++ src/test/java/y2020/sep/c206/Test1.java
@@ -0,0 +1,18 @@
+package y2020.sep.c206;
+
+import org.junit.Test;
+
+public class Test1 {
+
+  @Test
+  public void test() {
+    System.out.println(aplusb(10, 14));
+
+  }
+
+  /// you actual code goes below
+
+  public int aplusb(int a, int b) {
+    return a + b;
+  }
+}
